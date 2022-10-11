0e3bfc75218ea1fbb8b2c2ad90e7e70c2d279824
HazielMaldonado <92408661+HazielMaldonado@users.noreply.github.com>
2022-10-11 02:04:21 -0500

    Create Main.java

---
+++ ListaSimplementeLigada/Main.java
@@ -0,0 +1,55 @@
+package com.mycompany.listassimplementeligadas;
+
+/**
+ *
+ * @author hazie
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ListaSimplementeLigada<Integer> lista = new ListaSimplementeLigada<>();
+        
+        
+        System.out.println(lista);
+        
+        System.out.println(lista.estaVacia());
+        
+        System.out.println(lista.getTamanio());
+        
+        lista.agregarInicio(45);
+        System.out.println(lista);
+        lista.agregarInicio(30);
+        System.out.println(lista);
+        lista.agregarInicio(84);
+        System.out.println(lista);
+        lista.agregarInicio(11);
+        System.out.println(lista);
+        
+        lista.agregarAlfinal(28);
+        System.out.println(lista);
+        
+        lista.agregarDespues(30, 31);
+        System.out.println(lista);
+        
+        lista.eliminarPos(1);
+        System.out.println(lista);
+        
+        lista.EliminarPrimero();
+        System.out.println(lista);
+        
+        lista.EliminarFinal();
+        System.out.println(lista);
+        
+        System.out.println("El numero 30 esta en"+ lista.buscar(30));
+        
+        lista.actualizar(84, 99);
+        System.out.println(lista);
+        
+    }
+    
+    
+}
